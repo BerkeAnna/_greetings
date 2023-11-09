32eeb45415bd56084b98ad21d660cff96c2d6e30
BerkeAnna <berke.anna0803@gmail.com>
2023-11-09 21:06:16 +0100

    single input - test

--- tests/greeting.test.ts
+++ tests/greeting.test.ts
@@ -2,7 +2,11 @@ import { greetings } from "../src/greeting";
 
 describe('greetings', () => {
     test('no input', () => {
-    expect(greetings(null)).toBe('Hello, my friend');
+        expect(greetings(null)).toBe('Hello, my friend');
+    })
+
+    test('single input', () => {
+        expect(greetings(['Bob'])).toBe('Hello Bob.');
     })
 
 })
