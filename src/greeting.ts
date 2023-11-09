c5462edac0ee4dce0e7f4217ac743c9873d1fdc0
BerkeAnna <berke.anna0803@gmail.com>
2023-11-09 21:07:37 +0100

    single input - implement

--- src/greeting.ts
+++ src/greeting.ts
@@ -1,5 +1,7 @@
-export const greetings = (names : string | null) => {
+export const greetings = (names : string[] | null) => {
     if(names == null){
         return 'Hello, my friend';
+    }else if(names.length == 1){
+        return 'Hello ' + names + '.';
     }
 };
