49b1312d9d72d3b1dfd439980da2d2bfc0b26b97
BerkeAnna <berke.anna0803@gmail.com>
2023-11-09 21:22:27 +0100

    multiple input - test 2.0

--- tests/greeting.test.ts
+++ tests/greeting.test.ts
@@ -13,4 +13,8 @@ describe('greetings', () => {
         expect(greetings(['Alice', 'Bob', 'Jerry'])).toBe('Hello Alice, Bob and Jerry');
     })
 
+    test('multiple input 2.0', () => {
+        expect(greetings(['Alice', 'Bob', 'Jerry', 'Jenny'])).toBe('Hello Alice, Bob, Jerry and Jenny');
+    })
+
 })
