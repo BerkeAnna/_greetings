ae0ebbaa1e047e9b64d5c6db5e16139cf61f8245
BerkeAnna <berke.anna0803@gmail.com>
2023-11-09 21:21:19 +0100

    multiple input - refactor

--- src/greeting.ts
+++ src/greeting.ts
@@ -1,14 +1,11 @@
 export const greetings = (names : string[] | null) => {
-    if(names == null){
+    if(names === null){
         return 'Hello, my friend';
-    }else if(names.length == 1){
+    }else if(names.length === 1){
         return 'Hello ' + names + '.';
     }else{
-        var hi = new String('Hello ');
-        for(let i=0 ; i<names.length-2; i++){
-           hi += names[i] + ', ';
-        }
-        hi += names[names.length-2] + ' and ' + names[names.length-1];
-        return hi;
+        const allWithoutLast = names.slice(0, -1).join(', ');
+        const lastName = names[names.length - 1];
+        return `Hello ${allWithoutLast} and ${lastName}`;
     }
 };
