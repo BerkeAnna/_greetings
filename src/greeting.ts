4cf5f06f4ef99b54dc9e24cd2415353a419403b4
BerkeAnna <berke.anna0803@gmail.com>
2023-11-09 21:14:28 +0100

    multiple input -  implement

--- src/greeting.ts
+++ src/greeting.ts
@@ -3,5 +3,12 @@ export const greetings = (names : string[] | null) => {
         return 'Hello, my friend';
     }else if(names.length == 1){
         return 'Hello ' + names + '.';
+    }else{
+        var hi = new String('Hello ');
+        for(let i=0 ; i<names.length-2; i++){
+           hi += names[i] + ', ';
+        }
+        hi += names[names.length-2] + ' and ' + names[names.length-1];
+        return hi;
     }
 };
