0f03358b03406b68db25e73d603dc4bd2b621403
BerkeAnna <berke.anna0803@gmail.com>
2023-11-09 21:27:42 +0100

    only shouting input - implement

--- src/greeting.ts
+++ src/greeting.ts
@@ -2,7 +2,10 @@ export const greetings = (names : string[] | null) => {
     if(names === null){
         return 'Hello, my friend';
     }else if(names.length === 1){
-        return 'Hello ' + names + '.';
+        if(names[0] === names[0].toUpperCase()){
+            return 'HELLO ' + names[0] + '!' ;
+        }
+        return 'Hello ' + names[0] + '.';
     }else{
         const allWithoutLast = names.slice(0, -1).join(', ');
         const lastName = names[names.length - 1];
