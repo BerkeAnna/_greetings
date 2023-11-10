bd8e7e949a89710528b28e6c4305d27b578abe50
BerkeAnna <berke.anna0803@gmail.com>
2023-11-10 06:59:02 +0100

    Entry contains comma - implement

--- src/greeting.ts
+++ src/greeting.ts
@@ -1,41 +1,71 @@
 export const greetings = (names: string[] | null) => {
     if(names === null){
+
         return 'Hello, my friend';
+
     } else if(names.length === 1){
+
         if(names[0] === names[0].toUpperCase()){
             return 'HELLO ' + names[0] + '!';
         }
+
         return 'Hello ' + names[0] + '.';
+
     } else {
+
+        const separatedNames = names.flatMap(name => name.split(', ').filter(Boolean));
+        let itHasSeparatedNames = false
+
+
+        names.forEach(n => {
+
+            if (n.includes(', ')) {
+                itHasSeparatedNames = true;
+            }
+
+        });
+
         let upper: string[] = [];
         let lower: string[] = [];
 
-        for(let i = 0 ; i< names.length; i++){
-            if(names[i] == names[i].toUpperCase()){
-                upper.push(names[i]);
+        for(let i = 0 ; i< separatedNames.length; i++){
+
+            if(separatedNames[i] == separatedNames[i].toUpperCase()){
+                upper.push(separatedNames[i]);
             } else {
-                lower.push(names[i]);
+                lower.push(separatedNames[i]);
             }
+
         }
         let ret = "";
+
         if(lower.length > 0){
+
             const allWithoutLastLower = lower.slice(0, -1).join(', ');
             const lastLowerName = lower[lower.length - 1];
             ret = `Hello ${allWithoutLastLower} and ${lastLowerName}`;
-            if(upper.length > 0){
+
+            if(upper.length > 0 || itHasSeparatedNames){
+
                 ret += '.';
+
             }
         }
+
         if(upper.length > 0){
+
             if(lower.length > 0){
                 ret += ' HELLO ';
             } else {
                 ret += 'HELLO ';
             }
+
             const allWithoutLastUpper = upper.slice(0, -1).join(', ');
             const lastUpperName = upper[upper.length - 1];
             ret += `${allWithoutLastUpper} AND ${lastUpperName}!`;
+
         }
+
         return ret;
     }
 };
